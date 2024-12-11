1c7a20b04bdbe6f3d1728b7df0e8d4c0c7bc7ce2
kakimaru <memimei0306@gmail.com>
2024-12-12 04:38:11 +0900

    update styling

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -3,9 +3,10 @@ import styled from "styled-components";
 const Input = styled.input`
   border: 1px solid var(--color-grey-300);
   border-radius: var(--border-radius-sm);
-  border-color: var(--color-grey-0);
+  border-color: var(--color-grey-100);
   box-shadow: var(--shadow-sm);
   padding: 0.8rem 1.2rem;
+  color: var(--color-grey-900);
 `;
 
 export default Input;
