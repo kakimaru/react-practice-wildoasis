64ffe0b57d1c50bd52b9ae5b891e886417fe5bfe
kakimaru <memimei0306@gmail.com>
2024-12-12 05:44:20 +0900

    modify input color

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -6,7 +6,7 @@ const Input = styled.input`
   border-color: var(--color-grey-100);
   box-shadow: var(--shadow-sm);
   padding: 0.8rem 1.2rem;
-  color: var(--color-grey-900);
+  color: var(--color-grey-400);
 `;
 
 export default Input;
